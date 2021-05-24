240e08dbe73cf38ac395d3672db1d69f7278ee5a
Felipe-ce <felipe_rsr@live.com>
2021-05-24 20:12:05 -0300

    Update desafio 4

--- desafio 04/modules/nome-completo.js
+++ desafio 04/modules/nome-completo.js
@@ -1,25 +1,25 @@
 export default function initNomeCompleto() {
   // Pegar dados
-const nomeInput = document.querySelector('#nome')
-const sobreNomeInput = document.querySelector('#sobrenome')
-const btn = document.querySelector('#btn')
+  const nomeInput = document.querySelector('#nome')
+  const sobreNomeInput = document.querySelector('#sobrenome')
+  const btn = document.querySelector('#btn')
 
-function nomeCompleto() {
-  // Verificar se algum campo esta vazio
-  if(nomeInput.value.length == 0 || sobreNomeInput.value.length == 0){
-    alert(`[ERRO!] Verifique os dados`)
-  }else{
-    const nome = nomeInput.value
-    const sobreNome = sobreNomeInput.value
-    alert(`Nome Completo: ${nome} + ${sobreNome}`)
+  function nomeCompleto() {
+    // Verificar se algum campo esta vazio
+    if(nomeInput.value.length == 0 || sobreNomeInput.value.length == 0){
+      alert(`[ERRO!] Verifique os dados`)
+    }else{
+      const nome = nomeInput.value
+      const sobreNome = sobreNomeInput.value
+      alert(`Nome Completo: ${nome} ${sobreNome}`)
 
-    //limpar os input's
-    nomeInput.value = ''
-    sobreNomeInput.value = ''
-    // voltar para o primeiro input
-    nomeInput.focus()
+      //limpar os input's
+      nomeInput.value = ''
+      sobreNomeInput.value = ''
+      // voltar para o primeiro input
+      nomeInput.focus()
+    }
   }
-}
 
   btn.addEventListener('click', nomeCompleto) 
 }
