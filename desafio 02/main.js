bc02228c0b60df9304535d57aee67030e3d3fecf
Felipe-ce <felipe_rsr@live.com>
2021-05-24 19:35:52 -0300

    Update

--- desafio 02/main.js
+++ desafio 02/main.js
@@ -1,3 +1,21 @@
-import initSomar from './modules/somar.js'
+// Pegar dados
+const input1 = document.querySelector('#n1')
+const input2 = document.querySelector('#n2')
+const btn = document.querySelector('#btn')
 
-initSomar()
+const result = document.querySelector('#result')
+
+function somar() {
+  // Verificar se algum campo esta vazio
+  if(input1.value.length == 0 || input2.value.length == 0){
+    result.innerHTML = `<p>[ERRO!] Verifique os dados</p>`
+  }else{
+    const n1 = Number(parseInt(input1.value))
+    const n2 = Number(parseInt(input2.value))
+    const soma = n1 + n2
+    result.innerHTML = `A soma é: ${soma}`
+
+  }
+}
+
+btn.addEventListener('click', somar)
