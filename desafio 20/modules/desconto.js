b0efb9c21b7f4ea140a8605f47c395f64b4a6aa2
Felipe-ce <felipe_rsr@live.com>
2021-06-07 21:34:59 -0300

    Desafio 21 ok

--- desafio 20/modules/desconto.js
+++ desafio 20/modules/desconto.js
@@ -1,17 +1,18 @@
 export default function initDesconto() {
   // dados
   const numero1 = document.querySelector('#numero1')
-  // const numero2 = document.querySelector('#numero2')
   const result = document.querySelector('#result')
   const btn = document.querySelector('#btn')
   
   // função para desconto 
   function desconto() {
     const num1 = Number(numero1.value)
-    // const num2 = Number(numero2.value)
+    const valorDesconto = ((num1 * 5) / 100)
 
-  ( VTOTALLIQUIDO * DESCONTO1 ) / 100 
-    return result.innerHTML = ((num1 * 5) / 100)
+    // Converter o valor em Real Brasileiro
+    const valorDescontoReal = valorDesconto.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+
+    return result.innerHTML = valorDescontoReal
   }
   
   // evento de click
